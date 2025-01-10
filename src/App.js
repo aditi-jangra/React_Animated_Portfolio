e748c145b599a30f71f52da575ac827ce20e8a11
aditi-jangra <aditijangra504@gmail.com>
2025-01-10 17:16:29 +0530

    Save changes in App.js before pulling remote

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
-import Experience from './components/Experience'
+import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Aos from 'aos';
-import "aos/dist/aos.css"
+import "aos/dist/aos.css";
 import { useEffect } from 'react';
+import { getCLS, getFID, getLCP, getFCP, getTTFB } from 'web-vitals';
 
 const App = () => {
-  useEffect(()=>{
+  useEffect(() => {
     Aos.init();
-  }, [])
+
+    // Use individual web-vitals functions to log the metrics
+    getCLS(console.log); // Cumulative Layout Shift
+    getFID(console.log); // First Input Delay
+    getLCP(console.log); // Largest Contentful Paint
+    getFCP(console.log); // First Contentful Paint
+    getTTFB(console.log); // Time to First Byte
+  }, []);
 
   return (
     <div className="App">
-
-      <Navbar/>
-      <Home/>
-      <Experience/>
-      <Skills/>
-      <Projects/>
-      <Contact/>
-
+      <Navbar />
+      <Home />
+      <Experience />
+      <Skills />
+      <Projects />
+      <Contact />
     </div>
   );
 }
